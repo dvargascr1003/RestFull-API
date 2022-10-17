dac799361ae94bdfa3cd52caa91c685c6fcf2520
dvargascr1003 <115968417+dvargascr1003@users.noreply.github.com>
2022-10-16 22:44:59 -0600

    Add files via upload

---
+++ queries.js
@@ -0,0 +1,68 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
+})
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+const createUser = (request, response) => {
+  const { name, email, countries_id } = request.body
+
+  pool.query('INSERT INTO users (name, email, countries_id) VALUES ($1, $2, $3) RETURNING *', [name, email, countries_id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+  })
+}
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email, countries_id } = request.body
+
+  pool.query('UPDATE users SET name = $1, email = $2, countries_id = $3 WHERE id = $4',
+    [name, email, countries_id, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
+
